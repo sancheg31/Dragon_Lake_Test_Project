f151b82052985bc2f8d1b0415617da621ea5ad42
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-08-29 22:38:46 +0300

    EnemyObject refactoring

--- Dragon_Lake_Test/EnemyObject.cpp
+++ Dragon_Lake_Test/EnemyObject.cpp
@@ -1,2 +1,30 @@
+
 #include "EnemyObject.h"
 
+#include "CSprite.h"
+#include "CSpriteFactory.h"
+#include "ScreenArea.h"
+
+EnemyObject::EnemyObject(std::shared_ptr<MapArea> area, Point startPosition) : GameObject(area, startPosition) { }
+
+EnemyObject::~EnemyObject() { }
+
+ void EnemyObject::moveTo(Point position) {
+	if (isValidPosition(position))
+		mapPosition = position;
+}
+
+void EnemyObject::draw(const ScreenArea& area) const {
+	auto sprite = getSprite();
+	sprite->draw(mapPosition - area.currentShift());
+}
+
+std::shared_ptr<CSprite> EnemyObject::getSprite() const {
+	return spriteCreator->getSprite("enemy");
+}
+
+bool EnemyObject::isValidPosition(Point p) const {
+	auto [width, height] = mapArea->size();
+	auto [objw, objh] = size();
+	return (p.x >= 0 && p.y >= 0 && p.x <= width - objw && p.y <= height - objh);
+}
--- Dragon_Lake_Test/EnemyObject.h
+++ Dragon_Lake_Test/EnemyObject.h
@@ -2,37 +2,19 @@
 
 #include "GameObject.h"
 
-#include "CSpriteFactory.h"
-#include "Utility.h"
-#include "ScreenArea.h"
-
 class EnemyObject : public GameObject
 {
 public:
-	EnemyObject(const MapArea& area, Point startPosition) : GameObject(area, startPosition) { }
-	virtual ~EnemyObject() { }
+	EnemyObject(std::shared_ptr<MapArea> area, Point startPosition);
+	virtual ~EnemyObject();
 	
-	virtual void moveTo(Point position) {
-		if (isValidPosition(position))
-			mapPosition = position;
-	}
+	virtual void moveTo(Point position);
 
-	virtual void draw(const ScreenArea& area) const override {
-		auto sprite = getSprite();
-		sprite->draw(mapPosition - area.currentShift());
-	}
-
-	virtual void advance(const MoveStrategy&) override { }
+	virtual void draw(const ScreenArea& area) const override;
 
 protected:
-	virtual std::shared_ptr<CSprite> getSprite() const override {
-		return CSpriteFactory::instance()->getSprite("enemy");
-	}
+	virtual std::shared_ptr<CSprite> getSprite() const override;
 
-	virtual bool isValidPosition(Point p) const override {
-		auto [width, height] = mapArea.size();
-		auto [objw, objh] = size();
-		return (p.x >= 0 && p.y >= 0 && p.x <= width - objw && p.y <= height - objh);
-	}
+	virtual bool isValidPosition(Point p) const override;
 };
 
