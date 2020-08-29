c084a5771fbeb48db6c0a1d8ef6514a4800a393c
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-08-29 15:07:08 +0300

    operators for Point struct

--- Dragon_Lake_Test/Utility.h
+++ Dragon_Lake_Test/Utility.h
@@ -4,6 +4,26 @@ struct Point
 {
 	int x;
 	int y;
+
+	friend Point operator+(const Point& p1, const Point& p2) {
+		return Point{ p1.x + p2.x, p1.y + p2.y };
+	}
+
+	friend Point operator-(const Point& p1, const Point& p2) {
+		return Point{ p1.x - p2.x, p1.y - p2.y };
+	}
+
+	friend Point operator*(const Point& p, int val) {
+		return Point{ p.x * val, p.y * val };
+	}
+
+	friend Point operator*(int val, const Point& p) {
+		return Point{ p.x * val, p.y * val };
+	}
+
+	friend Point operator/(const Point& p, int val) {
+		return Point{ p.x * val, p.y * val };
+	}
 };
 
 struct Size
