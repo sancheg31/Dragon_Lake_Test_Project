b7f3ddb26b0278d42f39edb06f3d2930d6018221
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-08-29 22:38:25 +0300

    PlayerObject refactoting

--- Dragon_Lake_Test/PlayerObject.cpp
+++ Dragon_Lake_Test/PlayerObject.cpp
@@ -1,2 +1,31 @@
+
 #include "PlayerObject.h"
 
+#include "CSprite.h"
+#include "CSpriteFactory.h"
+#include "ScreenArea.h"
+
+PlayerObject::PlayerObject(std::shared_ptr<MapArea> area, Point startPosition) : GameObject(area, startPosition) { }
+
+PlayerObject::~PlayerObject() { }
+
+void PlayerObject::moveTo(Point position) {
+	if (isValidPosition(position))
+		mapPosition = position;
+}
+
+void PlayerObject::draw(const ScreenArea& area) const {
+	auto sprite = getSprite();
+	Point shiftedPosition = mapPosition - area.calculateScreenShift(*this);
+	sprite->draw(shiftedPosition);
+}
+
+std::shared_ptr<CSprite> PlayerObject::getSprite() const {
+	return spriteCreator->getSprite("player");
+}
+
+bool PlayerObject::isValidPosition(Point p) const {
+	auto [width, height] = mapArea->size();
+	auto [objw, objh] = size();
+	return (p.x >= 0 && p.y >= 0 && p.x <= width - objw && p.y <= height - objh);
+}
--- Dragon_Lake_Test/PlayerObject.h
+++ Dragon_Lake_Test/PlayerObject.h
@@ -1,41 +1,23 @@
 #pragma once
 
-#include "GameObject.h"
 
-#include "CSpriteFactory.h"
-#include "ScreenArea.h"
-#include "MapArea.h"
+#include "GameObject.h"
 
 class PlayerObject: public GameObject
 {
 public:
-	PlayerObject(const MapArea& area, Point startPosition) : GameObject(area, startPosition) { }
-	virtual ~PlayerObject() { }
+	PlayerObject(std::shared_ptr<MapArea> area, Point startPosition);
+	virtual ~PlayerObject();
 
-	virtual void moveTo(Point position) override {
-		if (isValidPosition(position))
-			mapPosition = position;
-	}
+	virtual void moveTo(Point position) override;
 
-	virtual void draw(const ScreenArea& area) const override {
-		auto sprite = getSprite();
-		Point shiftedPosition = mapPosition - area.calculateScreenShift(*this);
-		sprite->draw(shiftedPosition);
-	}
-
-	virtual void advance(const MoveStrategy&) override { }
+	virtual void draw(const ScreenArea& area) const override;
 
 protected:
 
-	virtual std::shared_ptr<CSprite> getSprite() const override {
-		return CSpriteFactory::instance()->getSprite("player");
-	}
+	virtual std::shared_ptr<CSprite> getSprite() const override;
 
-	virtual bool isValidPosition(Point p) const override {
-		auto [width, height] = mapArea.size();
-		auto [objw, objh] = size();
-		return (p.x >= 0 && p.y >= 0 && p.x <= width - objw && p.y <= height - objh);
-	}
+	virtual bool isValidPosition(Point p) const override;
 
 };
 
