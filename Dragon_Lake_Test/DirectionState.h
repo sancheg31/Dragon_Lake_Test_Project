37b225c7595b7573532971229ea912646599ce24
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-09-02 12:08:24 +0300

    DirectionState class for unit vectors

---
+++ Dragon_Lake_Test/DirectionState.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+
+#include "LinearTrajectoryGenerator.h"
+#include "Utility.h"
+
+#include <cmath>
+
+
+double distance(Point p1, Point p2) {
+	return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+}
+
+struct Rotation
+{
+public:
+	double x1, y1;
+	double x2, y2;
+};
+
+class DirectionState
+{
+public:
+	DirectionState(Point p): state(normalize(p)) { }
+
+	DirectionState left() {
+		auto p = Point{ (int)(state.x * leftRotation.x1 + state.y * leftRotation.y1),
+						(int)(state.x * leftRotation.x2 + state.y * leftRotation.y2) };
+		return normalize(p);
+	}
+
+	DirectionState right() {
+		auto p = Point{ (int)(state.x * rightRotation.x1 + state.y * rightRotation.y1),
+						(int)(state.x * rightRotation.x2 + state.y * rightRotation.y2)};
+		return normalize(p);
+	}
+
+private:
+
+	Point normalize(Point p) {
+		if (p.x != 0)
+			p.x = 1 * sign(p.x);
+		if (p.y != 0)
+			p.y = 1 * sign(p.y);
+	}
+
+	Point state;
+	Rotation leftRotation{ sqrt(2) / 2, sqrt(2) / 2, -sqrt(2) / 2, sqrt(2) / 2 };
+	Rotation rightRotation{ sqrt(2) / 2, -sqrt(2) / 2, sqrt(2) / 2, sqrt(2) / 2 };
+};
